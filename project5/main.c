288988962a03814b0f7977e88b05d8f7240907a1
xiaoyaokeyx <1017346331@qq.com>
2021-04-01 00:31:45 +0800

    Initial Commit

---
+++ project5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  project5
+//
+//  Created by yx on 2021/4/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
